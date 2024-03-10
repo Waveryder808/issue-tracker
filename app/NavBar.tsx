56bc9008bb28f63bac02e433953112f9c9047f2f
waveryder808 <ohsmith81@gmail.com>
2024-03-09 22:18:19 -0800

    dependency's changed + minor navbar tweak

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname  } from 'next/navigation';
 import classnames from 'classnames';
 import { DiCodeigniter } from 'react-icons/di';
 import { HiRefresh } from 'react-icons/hi';
